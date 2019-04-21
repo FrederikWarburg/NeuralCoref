38e355e21ce3a402050ed1e713539d11abc23183
frederikwarburg <frewar1905@gmail.com>
2019-04-21 11:59:00 -0700

    f1 pred and recall implemented

--- calculatescore.py
+++ calculatescore.py
@@ -20,3 +20,19 @@ def calculate_score(answers_path, submission_path):
     temp = np.multiply(submission, y)
 
     return np.sum(temp)/-submission.shape[0]
+
+
+def evaluate(gold_path, predictions_path):
+    # https://scikit-learn.org/stable/modules/generated/sklearn.metrics.f1_score.html
+    # different ways of averaging
+    predictions = pd.read_csv(predictions_path)
+    predictions = predictions.values[:,1:].astype(int)
+    
+    gold = pd.read_csv(gold_path)
+    gold = gold.values[:,1:].astype(int)
+
+    f1 = f1_score(gold, predictions, average=None)
+    precision = precision_score(y_true, y_pred, average=None)
+    recall = recall_score(y_true, y_pred, average=None)
+    
+    return precision, recall, f1
